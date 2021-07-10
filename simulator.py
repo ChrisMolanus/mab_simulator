8842a2965fca8a8a392365c6949eefe45f2a5955
Chris Molanus <chrismolanus@gmail.com>
2021-07-10 16:07:09 +0200

    feature: generate actions and customers for sim

--- simulator.py
+++ simulator.py
@@ -1,24 +1,36 @@
+import csv
+from datetime import datetime
 from typing import Dict, List
 from multiprocessing import Process
 
 import dashingRingtail
 import fierceCrayfish
-from policy import ServedActionPropensity, Policy, Customer
+from actionGenerator import get_actions
+from customerGenerator import generate_customers
+from policy import ServedActionPropensity, Policy, Customer, Address, Product, Channel, Action, Offer
 
 
-def policy_sim(policy, customers: List[Customer]):
-    # policy = policy
-    # customers = customers
 
-    sservedActionPropensities: Dict[int, ServedActionPropensity] = dict()
+def policy_sim(policy_class, customers: List[Customer], actions: List[Action]):
+    print(policy_class.__name__)
+    policy = policy_class()
 
-def generate_customers() -> List[Customer]:
+
+
+
+    servedActionPropensities: Dict[int, ServedActionPropensity] = dict()
+
+
+
 
 
 if __name__ == "__main__":
-    policies = [fierceCrayfish, dashingRingtail]
+    policies = [fierceCrayfish.FierceCrayfish, dashingRingtail.DashingRingtail]
 
     processes = list()
-    customers = list()
-    for policy in policies:
-        processes.append(Process(target=policy_sim, args=(policy, customers)))
+    customers = generate_customers(10000)
+    actions = get_actions()
+    for policy_class in policies:
+        p = Process(target=policy_sim, args=(policy_class, customers, actions))
+        p.start()
+        processes.append(p)
