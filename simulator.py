a0ba970361810ff1f15719a5e70413bc826a362f
Chris Molanus <chrismolanus@gmail.com>
2021-07-11 14:16:53 +0200

    fix: some bugs

--- simulator.py
+++ simulator.py
@@ -1,7 +1,10 @@
 import csv
 from datetime import datetime, timedelta
-from typing import Dict, List
-from multiprocessing import Process
+from typing import Dict, List, Any
+from multiprocessing import Process, Queue
+
+from pandas import DataFrame
+import numpy as np
 
 import dashingRingtail
 import fierceCrayfish
@@ -11,14 +14,17 @@ from policy import ServedActionPropensity, Policy, Customer, Address, Product, C
 from rewardCalculator import RewardCalculator
 
 
-def policy_sim(policy_class, customers: List[Customer], actions: List[Action], day_count: int):
+def policy_sim(policy_class, customers: List[Customer], actions: List[Action], day_count: int, output: Queue) -> DataFrame:
     print(policy_class.__name__)
     policy: Policy = policy_class()
     rewardCalculator = RewardCalculator()
 
+    log: List[Dict[str, Any]] = list()
+
     servedActionPropensities: Dict[int, ServedActionPropensity] = dict()
     actionTimeout: Dict[datetime, dict[int, ServedActionPropensity]] = dict()
 
+    cumulative_reward = 0.0
     start_date = datetime.today()
     for today_datetime in (start_date + timedelta(n) for n in range(day_count)):
         today = today_datetime.date()
@@ -58,10 +64,11 @@ def policy_sim(policy_class, customers: List[Customer], actions: List[Action], d
                 del actionTimeout[deadline][customer.id]
                 del servedActionPropensities[customer.id]
 
+                cumulative_reward += reward
 
-
-
-
+        log.append({"ts": today, "cumulative_reward": cumulative_reward})
+        print(today)
+    output.put({"policy": policy_class.__name__, "logs": [log]})
 
 
 if __name__ == "__main__":
@@ -70,10 +77,34 @@ if __name__ == "__main__":
     processes = list()
     customers = generate_customers(10000)
     actions = get_actions()
+    output_queue = Queue()
     for policy_class in policies:
-        p = Process(target=policy_sim, args=(policy_class, customers, actions, 365))
+        p = Process(target=policy_sim, args=(policy_class, customers, actions, 365, output_queue))
         p.start()
         processes.append(p)
 
+    all_logs: Dict[str, Dict[datetime, List[float]]] = dict()
+    plot_dict: Dict[str, List[Dict[datetime, dict]]] = dict()
+    for policy_class in policies:
+        all_logs[policy_class] = dict()
+        plot_dict[policy_class] = dict()
+
     for p in processes:
-        p.join()
+        #p.join()
+        output_logs = output_queue.get(block=True)
+        #for policy_name, run_logs in logs.items():
+        logs = output_logs["logs"]
+        policy_name = output_logs["policy"]
+        for log in logs:
+            for ts, cum_reward in log:
+                #ts = log["ts"]
+                if ts not in all_logs[policy_name]:
+                    all_logs[policy_name][ts] = list()
+                all_logs[policy_name][ts].append(cum_reward)
+
+    plot_dfs: Dict[str, DataFrame] = dict()
+    for policy, log in all_logs.items():
+        for ts, sim_values in log.items():
+            plot_dict[policy].append({"ts": ts, "mean": np.mean(sim_values), "std": np.std(sim_values)})
+        plot_dfs[policy] = DataFrame(plot_dict[policy])
+
