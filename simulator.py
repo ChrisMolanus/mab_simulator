e547f506c7fadb6f5676a9631a426a1032e1cc95
Chris Molanus <chrismolanus@gmail.com>
2021-07-12 09:30:01 +0200

    feature: add plot of cumalative rewards

--- simulator.py
+++ simulator.py
@@ -5,6 +5,7 @@ from multiprocessing import Process, Queue
 
 from pandas import DataFrame
 import numpy as np
+import matplotlib.pyplot as plt
 
 import dashingRingtail
 import fierceCrayfish
@@ -109,4 +110,16 @@ if __name__ == "__main__":
         for ts, sim_values in log.items():
             plot_dict[policy].append({"ts": ts, "mean": np.mean(sim_values), "std": np.std(sim_values)})
         plot_dfs[policy] = DataFrame(plot_dict[policy])
+
+    fig, ax = plt.subplots()
+    for policy_name, policy in plot_dfs.items():
+        ax.plot(policy["ts"], policy["mean"])
+
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+           title='About as simple as it gets, folks')
+    ax.grid()
+
+    fig.savefig("test.png")
+    plt.show()
+
 
