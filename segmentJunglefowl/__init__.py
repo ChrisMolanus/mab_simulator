0873cd5fe7a36c1539c1a128a0fdade0c0faaa9d
Chris Molanus <chris.molanus@cognizant.com>
2021-12-10 15:51:36 +0100

    feature: accualy get segmentJunglefowl working

--- segmentJunglefowl/__init__.py
+++ segmentJunglefowl/__init__.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
-from typing import Dict, List, Set, Optional
+from typing import Dict, List, Set, Optional, Tuple
 
+import numpy as np
 from numpy import random
 
 from customerGenerator import get_products
-from policy import Policy, Action, ServedActionPropensity, CustomerAction, Customer, Channel
+from policy import Policy, Action, ServedActionPropensity, CustomerAction, Customer, Channel, Product
 from rewardCalculator import HlvCalculator
 from segmentJunglefowl.marketingSegment import GoldSilverBronze, GoldSilverBronzeSegment
 
@@ -12,7 +13,7 @@ from segmentJunglefowl.marketingSegment import GoldSilverBronze, GoldSilverBronz
 
 
 class SegmentJunglefowl(Policy):
-    def __init__(self, **kwargs):
+    def __init__(self, current_base, **kwargs):
         """
         Simulates the logic of a marketing department using a Gold, Silver, Bronze segmenting system.
         Gold customers are ones that have the highest current margin.
@@ -27,26 +28,67 @@ class SegmentJunglefowl(Policy):
         We can perform a Up-Sell or a Cross-Sell
         If both a Up and Cross Sell can be performed we choose one at random.
         Of the actions possible in the Cross/Up Sell we choose a action at random.
+
+        :param current_base: The current install base
         """
         self.now_ts = datetime.now()
         self.last_updated = datetime.now() - timedelta(days=1)
 
         self.segment_actions: Dict[str, List[Action]] = dict()
-
-        product_list, _ = get_products()
-        self.products = set(product_list)
-        self.segmentor = GoldSilverBronze()
-        self.hlv_calculator = HlvCalculator()
+        self.current_base = current_base
+        self.all_segment_actions: Dict[int, Set[Action]] = dict()
+        self.segmentor = GoldSilverBronze(self.last_updated, self.current_base)
+        self.segment_actions: Dict[GoldSilverBronzeSegment, Dict[int, Set[Action]]] = {
+            GoldSilverBronzeSegment.Gold: dict(),
+            GoldSilverBronzeSegment.Silver: dict(),
+            GoldSilverBronzeSegment.Bronze: dict(),
+        }
+        self.actions: List[Action] = list()
 
     def add_arm(self, action: Action, segment_ids: List[str]):
-        for segment_id in segment_ids:
-            if segment_id not in self.segment_actions:
-                self.segment_actions[segment_id] = list()
-            self.segment_actions[segment_id].append(action)
+        self.actions.append(action)
 
-        for product in action.offer.products:
-            if product not in self.products:
-                self.products.add(product)
+        for segment_id in segment_ids:
+            if segment_id not in self.all_segment_actions:
+                self.all_segment_actions[segment_id] = list()
+            self.all_segment_actions[segment_id].append(action)
+
+        self.rebalance_product_segments()
+
+    def rebalance_product_segments(self):
+        # Re-balance product segmentation
+        margins: Dict[float, Set[Tuple[int, Action]]] = dict()
+        for segment_id, actions in self.all_segment_actions.items():
+            for action_1 in actions:
+                margin = action_1.get_max_margin()
+                if margin not in margins:
+                    margins[margin] = set()
+                margins[margin].add((segment_id, action_1))
+
+        triple = np.percentile(list(margins.keys()), [20, 80])
+
+        self.silver_product_threshold = triple[0]
+        self.gold_product_threshold = triple[1]
+        self.segment_actions: Dict[GoldSilverBronzeSegment, Dict[int, Set[Action]]] = {
+            GoldSilverBronzeSegment.Gold: dict(),
+            GoldSilverBronzeSegment.Silver: dict(),
+            GoldSilverBronzeSegment.Bronze: dict(),
+        }
+
+        for margin, pairs in margins.items():
+            for segment_id, action_1 in pairs:
+                if margin > self.gold_product_threshold:
+                    if segment_id not in self.segment_actions[GoldSilverBronzeSegment.Gold]:
+                        self.segment_actions[GoldSilverBronzeSegment.Gold][segment_id] = set()
+                    self.segment_actions[GoldSilverBronzeSegment.Gold][segment_id].add(action_1)
+                elif margin > self.silver_product_threshold:
+                    if segment_id not in self.segment_actions[GoldSilverBronzeSegment.Silver]:
+                        self.segment_actions[GoldSilverBronzeSegment.Silver][segment_id] = set()
+                    self.segment_actions[GoldSilverBronzeSegment.Silver][segment_id].add(action_1)
+                else:
+                    if segment_id not in self.segment_actions[GoldSilverBronzeSegment.Bronze]:
+                        self.segment_actions[GoldSilverBronzeSegment.Bronze][segment_id] = set()
+                    self.segment_actions[GoldSilverBronzeSegment.Bronze][segment_id].add(action_1)
 
     def add_customer_action(self, served_action_propensity: ServedActionPropensity, customer_action: CustomerAction,
                             reward: float):
@@ -59,46 +101,48 @@ class SegmentJunglefowl(Policy):
         pass
 
     def set_datetime(self, now_ts: datetime):
-        for segment_id, segment_actions in self.segment_actions.items():
-            indexes_to_remove = list()
-            for i in range(0, len(segment_actions)):
-                if segment_actions[i].end_date <= now_ts.date():
-                    indexes_to_remove.append(i)
+        action_removed = False
+        for our_segment_id, segmented_actions in self.segment_actions.items():
+            for segment_id, segment_actions in segmented_actions.items():
+                actions_to_remove: List[Action] = list()
+                for action in segment_actions:
+                    if action.end_date <= now_ts.date():
+                        actions_to_remove.add(action)
 
-            indexes_to_remove.sort(reverse=True)
-            if len(indexes_to_remove) != len(self.segment_actions[segment_id]):
-                for i in indexes_to_remove:
-                    del self.segment_actions[segment_id][i]
-            else:
-                del self.segment_actions[segment_id]
+                if len(actions_to_remove) > 0:
+                    for acton in actions_to_remove:
+                        segment_actions.remove(acton)
+                    action_removed = True
+                # TODO: clean up empty segment_id
+
+        if action_removed:
+            self.rebalance_product_segments()
 
         self.now_ts = now_ts
         self.last_updated = self.now_ts.date()
 
     def get_next_best_action(self, customer: Customer, segment_ids: List[str]) -> Optional[ServedActionPropensity]:
-
-
-
-        random_action = random.choice(self.segment_actions[random_segment])
-        propensities: Dict[str, float] = dict()
-        for segment_id, segment_actions in self.segment_actions.items():
-            for action in segment_actions:
-                propensities[action.name] = (1.0 / len(intersecting_segments)) * (1.0 / len(segment_actions))
-
-        return ServedActionPropensity(customer=customer, chosen_action=random_action,
-                                      action_propensities=propensities)
-
-    def get_action_from_segment(self, customer: Customer, their_segments: List[str]):
-        intersecting_segments = set(their_segments).intersection(self.segment_actions.keys())
-        allowed_actions: Dict[GoldSilverBronzeSegment, Set[Action]] = dict()
-        for segment_id in intersecting_segments:
-            for segment_actions in self.segment_actions[segment_id]:
-                margin = 0.0
-                for product in segment_actions.offer:
-                    margin += product.get_margin()
-                allowed_actions.union(set(segment_actions))
-
-
         our_segment = self.segmentor.get_segments_for_customer(customer, self.last_updated)
-        if our_segment == GoldSilverBronzeSegment.Gold:
+        intersecting_segments = set(segment_ids).intersection(self.segment_actions[our_segment].keys())
+
+        allowed_actions: Set[Action] = set()
+        for segment_id in intersecting_segments:
+            allowed_actions = allowed_actions.union(self.segment_actions[our_segment][segment_id])
+
+        if len(allowed_actions) > 0:
+            random_segment_action = random.choice(list(allowed_actions))
+            prob = 1 / len(allowed_actions)
+            propensities: Dict[str, float] = dict()
+            for our_segment_id, segmented_actions in self.segment_actions.items():
+                for segment_id, segment_actions in segmented_actions.items():
+                    for action in segment_actions:
+                        propensities[action.name] = prob if action in allowed_actions else 0.0
+
+            return ServedActionPropensity(customer=customer, chosen_action=random_segment_action,
+                                          action_propensities=propensities)
+        else:
+            return None
+
+
+
 
--- segmentJunglefowl/marketingSegment.py
+++ segmentJunglefowl/marketingSegment.py
@@ -2,6 +2,8 @@ from datetime import date
 from enum import Enum
 from typing import Dict, List
 
+import numpy as np
+
 from policy import Customer, Product
 from rewardCalculator import HlvCalculator
 
@@ -30,12 +32,48 @@ class CustomerSegmentor:
 
 
 class GoldSilverBronze(CustomerSegmentor):
-    def __init__(self, gold_threshold: float = 300.0, silver_threshold: float = 200.0):
-        self.gold_threshold = gold_threshold
-        self.silver_threshold = silver_threshold
+    def __init__(self, context_date: date, current_base: List[Customer] = None, gold_threshold: float = None, silver_threshold: float = None):
+        """
+        A Gold, Silver, Bronze market segmentation model
+        :param context_date: The date use to calculate the HLV of the customers
+        :param current_base: The current base
+        :param gold_threshold: The value to set the Gold threshold to and ignore the customer base (Optional)
+        :param silver_threshold: The value to set the Silver threshold to and ignore the customer base (Optional)
+        """
         self.hlv_calculator = HlvCalculator()
 
+        if gold_threshold is not None and silver_threshold is not None:
+            self.gold_threshold = gold_threshold
+            self.silver_threshold = silver_threshold
+        elif current_base is None or len(current_base) == 0:
+            self.gold_threshold = 300.0
+            self.silver_threshold = 200.0
+        else:
+            self.calibrate(context_date, current_base)
+
+    def calibrate(self, context_date: date, current_base: List[Customer]):
+        """
+        Calibrate the Gold and Silver thresholds based on the current base.
+        Just hack it up into three segments based on absolute customer margin (ignore density)
+        :param context_date: The date use to calculate the HLV of the customers
+        :param current_base: The current base
+        """
+        margins = list()
+        for customer in current_base:
+            margins.append(self.hlv_calculator.get_hlv(customer, context_date))
+        triple = np.percentile(margins, [20, 80])
+
+        self.silver_threshold = triple[0]
+        self.gold_threshold = triple[1]
+
     def get_segments_for_customer(self, customer: Customer, context_date: date) -> GoldSilverBronzeSegment:
+        """
+        Get the segment the customer should be in.
+        Since the expected margin of a customer depends on how far into a contract they are the context_date is needed
+        :param customer: The customer object
+        :param context_date: The date at which to calculate the customers expected margin
+        :return:
+        """
         margin = self.hlv_calculator.get_hlv(customer, context_date)
         if margin > self.gold_threshold:
             return GoldSilverBronzeSegment.Gold
