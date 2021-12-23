c8170a38c40a47edb678c6d7f745d648a8d1f279
Chris Molanus <chris.molanus@cognizant.com>
2021-12-23 17:32:30 +0100

    feature: export history data as parket files

--- historyGenerator.py
+++ historyGenerator.py
@@ -1,13 +1,16 @@
 import datetime
 import json
+import os
 from random import seed
 
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
 import segmentJunglefowl
 from actionGenerator import get_actions
 from customerGenerator import generate_customers, get_products
+from policy import get_channel_action_cost
 from rewardCalculator import RewardCalculator
 from simulator import sim_cycle_run
 
@@ -32,5 +35,64 @@ keywords = {"current_base": all_customers}
 all_actions = get_actions()
 
 log, chosen_action_log, historicalActionPropensities = sim_cycle_run(all_actions, all_customers, day_count,
-                                                                     keywords, segmentJunglefowl.SegmentJunglefowl,
-                                                                     reward_calculator, start_date)
+                                                                     segmentJunglefowl.SegmentJunglefowl,
+                                                                     reward_calculator, [], start_ts, **keywords)
+output_dir = "output"
+
+# Export customer
+customer_list = list()
+for customer in all_customers:
+    customer_list.append(
+        {
+            "id":customer.id,
+            "name": customer.name,
+            "dob": customer.dob,
+            "billing_postcode": customer.billing_address.postcode,
+            "billing_house_number": customer.billing_address.house_number
+        }
+    )
+customers = pd.DataFrame(customer_list)
+customers.to_parquet(os.path.join(output_dir, "customers.parquet"), index=False)
+del customers
+
+# Export actions
+action_list = list()
+action_product_list = list()
+for action in all_actions:
+    action_list.append({
+        "name": action.name,
+        "start_date": action.start_date,
+        "end_date": action.end_date,
+        "cool_off_days": action.cool_off_days,
+        "channel": str(action.channel),
+        "offer_name": action.offer.name,
+        "max_margin": action.get_max_margin(years_horizon=5),
+        "cost": get_channel_action_cost(action.channel)
+    })
+    for product in action.offer.products:
+        action_product_list.append({
+            "action_name": action.name,
+            "product_name": product.name
+        })
+actions = pd.DataFrame(action_list)
+actions.to_parquet(os.path.join(output_dir, "actions.parquet"), index=False)
+del actions
+
+# Export Product / Action link
+action_product = pd.DataFrame(action_product_list)
+action_product.to_parquet(os.path.join(output_dir, "action_product.parquet"), index=False)
+del action_product
+
+# Export Transactions
+transaction_list = list()
+for t in historicalActionPropensities:
+    transaction_list.append({
+        "id": t.customer.id,
+        "action_ts": t.action_ts,
+        "action_name": t.chosen_action.name,
+        "reward_ts": t.reward_ts,
+        "reward": t.reward,
+    })
+transactions = pd.DataFrame(transaction_list)
+transactions.to_parquet(os.path.join(output_dir, "transactions.parquet"), index=False)
+del transactions
